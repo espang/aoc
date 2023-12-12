43afd4cda4bdcce6908c606deb9ef8de789427a1
Eike Spang <eike.spang@gmail.com>
2023-12-12 20:36:27 +0000

    day12 y2023 ocaml

--- y2023/ocaml/day12.ml
+++ y2023/ocaml/day12.ml
@@ -25,48 +25,6 @@ let parse_line l =
   in
   (condition_records, damaged_springs)
 
-let number_of_solutions_for (condition_records, damaged_springs) =
-  let handle_operational counter damaged_counts =
-    if counter = 0
-    then Some damaged_counts
-    else
-      match damaged_counts with
-      | [] -> None
-      | v::tl ->
-        if v = counter
-        then Some tl
-        else None
-  in
-  let rec count_solutions counter damaged condition_records =
-    if List.is_empty damaged && counter > 0 then 0 else
-    if counter > 0 && List.hd_exn damaged < counter then 0 else
-    match condition_records with
-    | [] ->
-      if counter > 0
-      then if List.length damaged = 1 && List.hd_exn damaged = counter then 1 else 0
-      else if List.is_empty damaged then 1 else 0
-    | hd::tl ->
-      match hd with
-      | Damaged -> count_solutions (counter + 1) damaged tl
-      | Operational -> 
-        (match handle_operational counter damaged with
-        | Some damaged' -> count_solutions 0 damaged' tl
-        | None -> 0)
-      | Unknown ->
-        (count_solutions (counter + 1) damaged tl +
-        match handle_operational counter damaged with
-        | Some damaged' -> count_solutions 0 damaged' tl
-        | None -> 0)
-  in
-  count_solutions 0 damaged_springs condition_records
-
-let part1 content = 
-  String.split_lines content
-  |> List.map ~f:parse_line
-  |> List.map ~f:number_of_solutions_for
-  |> List.fold ~init:0 ~f:(+)
-  |> Printf.printf "%d\n"
-
 let repeat5 lst =
   List.append lst lst
   |> List.append lst
@@ -74,27 +32,90 @@ let repeat5 lst =
   |> List.append lst
 
 let repeat5_with_unknown lst = 
-  List.join [lst; [Unknown]; lst;[Unknown]; lst;[Unknown]; lst;[Unknown]; lst]
+  List.join [
+    lst; [Unknown];
+    lst; [Unknown];
+    lst; [Unknown];
+    lst; [Unknown];
+    lst
+  ]
 
 let extend (condition_records, damaged_springs) =
   (repeat5_with_unknown condition_records, repeat5 damaged_springs)
+
+module Pair = struct
+  module T = struct
+    type t = spring list * int list
+
+    let rec list_equal eq coll1 coll2 =
+      match coll1, coll2 with
+      | [], [] -> true
+      | _,  [] -> false
+      | [], _  -> false
+      | (v1::cs1), (v2::cs2) ->
+        if eq v1 v2
+        then list_equal eq cs1 cs2
+        else false
+    let compare (cs1, ds1) (cs2, ds2) =
+      if list_equal (=) ds1 ds2 && list_equal phys_equal cs1 cs2
+      then 0
+      else 1
+    let sexp_of_t = 
+      Tuple2.sexp_of_t (List.sexp_of_t sexp_of_opaque)  (List.sexp_of_t Int.sexp_of_t)
+    
+    let t_of_sexp = Tuple2.t_of_sexp (List.t_of_sexp opaque_of_sexp) (List.t_of_sexp Int.t_of_sexp)
+  end
+
+  include T
+  include Comparable.Make(T)
+end
+
+let solve (condition_records, damaged_springs) =
+  let is_operational v = phys_equal v Operational in
+  let possible number conditions =
+    if List.length conditions >= number
+    then 
+      match List.nth conditions number with
+      | Some Damaged -> false
+      | _ -> List.take conditions number |> List.exists ~f:is_operational |> not
+    else false
+  in
+  let memory = ref (Map.empty (module Pair)) in
+  let rec f = function 
+    | ([], [])   -> 1
+    | (cs, []) -> if List.exists cs ~f:(phys_equal Damaged) then 0 else 1
+    | ([], _)  -> 0
+    | (conds, dmgs) as t ->
+      if Map.mem !memory t
+      then Map.find_exn !memory t
+      else
+        let next_number = List.hd_exn dmgs in
+        let data =
+          match (List.hd_exn conds), possible next_number conds with
+          | Operational, _
+          | Unknown, false -> f (List.tl_exn conds, dmgs)
+          | Damaged, true  -> f (List.drop conds (succ next_number), List.tl_exn dmgs)
+          | Damaged, false -> 0
+          | Unknown, true  -> 
+            f (List.drop conds (succ next_number), List.tl_exn dmgs)
+            + f (List.tl_exn conds, dmgs)
+        in
+        memory := Map.add_exn !memory ~key:t ~data;
+        data
+  in
+  f (condition_records, damaged_springs)
+
+let part1 content = 
+  String.split_lines content
+  |> List.map ~f:parse_line
+  |> List.map ~f:solve
+  |> List.fold ~init:0 ~f:(+)
+  |> Printf.printf "%d\n"
 
 let part2 content =
   String.split_lines content
   |> List.map ~f:parse_line
   |> List.map ~f:extend
-  |> (fun coll -> List.take coll 10)
-  |> List.map ~f:number_of_solutions_for
+  |> List.map ~f:solve
   |> List.fold ~init:0 ~f:(+)
   |> Printf.printf "%d\n"
-
-(* 
-(* ?#??.??..##?.???#.? 4,1,2,1,1,1 *)
-(* 1-4.0-2. 2-3 .1-4.
-   1-6.0-2. 2-3 .1-4.
-   1-6.0-2. 2-3 .1-4.
-   1-6.0-2. 2-3 .1-4.
-   1-6.0-2. 2-3 .1-4.
-*)
-
-?#.#???#???.??.???? 2,2,1,1,2,1 *)
