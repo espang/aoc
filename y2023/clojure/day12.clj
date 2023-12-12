29c22f547049d0f46c1aa881bd50fdf7ac09453e
Eike Spang <eike.spang@gmail.com>
2023-12-12 18:46:38 +0000

    refactor day12

--- y2023/clojure/day12.clj
+++ y2023/clojure/day12.clj
@@ -1,54 +1,47 @@
 (require '[clojure.string :as str])
 
-(defn str->number-list [s] (map #(Integer/parseInt %) (str/split s #",")))
-
 (defn parse-line [l times]
-  (let [[condition damage] (str/split l #" " 2)
+  (let [str->number-list (fn [s] (map #(Integer/parseInt %) (str/split s #",")))
+        [condition damage] (str/split l #" " 2)
         condition (str/join "?" (repeat times condition))
         damage    (str/join "," (repeat times damage))]
     [condition (str->number-list damage)]))
 
-(defn is-operational? [c] (= c \.))
-(defn is-damaged? [c] (= c \#))
-
-(defn can-be?
+(defn possible?
+  "checks wether the collection can start with number damaged entries from the start."
   [coll number]
   (when (>= (count coll) number)
-    (and (not (some is-operational? (take number coll)))
+    (and (not (some #(= % \.) (take number coll)))
          (not (= (nth coll number \.) \#)))))
 
 (def m-f
   (memoize
    (fn [condition damage]
      (if-not (seq damage)
-       (if (some is-damaged? condition)
-         0
-         1)
+       (if (some #(= % \#) condition) 0 1)
        (if-not (seq condition)
          0
          (let [number (first damage)]
-           (case (first condition)
-             \# (if (can-be? condition number)
-                  (m-f (drop (inc number) condition) (rest damage))
-                  0)
-             \? (if (can-be? condition number)
-                  (+ (m-f (drop (inc number) condition) (rest damage))
-                     (m-f (rest condition) damage))
-                  (m-f (rest condition) damage))
-             \. (m-f (rest condition) damage))))))))
+           (if (possible? condition number)
+             (case (first condition)
+               \# (m-f (drop (inc number) condition) (rest damage))
+               \? (+ (m-f (drop (inc number) condition) (rest damage))
+                     (m-f (rest condition) damage)))
+             (case (first condition)
+               \# 0
+               (m-f (rest condition) damage)))))))))
+
+(defn solve [content times]
+  (->> content
+       str/split-lines
+       (map #(parse-line % times))
+       (map (fn [[condition damage]]
+              (m-f condition damage)))
+       (reduce + 0)))
 
 (comment
   (def content (slurp "../../inputs/2023_12.txt"))
   ;; part1
-  (time (->> content
-             str/split-lines
-             (map #(parse-line % 1))
-             (map (fn [[condition damage]]
-                    (m-f condition damage)))
-             (reduce + 0)))
-  (time (->> content
-             str/split-lines
-             (map #(parse-line % 5))
-             (map (fn [[condition damage]]
-                    (m-f condition damage)))
-             (reduce + 0))))
+  (time (solve content 1))
+  ;; part2
+  (time (solve content 5)))
